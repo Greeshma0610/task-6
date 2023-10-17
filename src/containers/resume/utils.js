72ae3e84809dee5af37ad0aeb390056fc11daeda
Greeshma0610 <140088691+Greeshma0610@users.noreply.github.com>
2023-10-17 20:02:13 +0530

    Add files via upload

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,28 @@
+export const data = {
+    experience: [
+      {
+        title: "Consultant",
+        subTitle: "USA",
+        description:
+          " User Experience, Visual Design",
+      },
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "Sri Chaitanya School",
+        description:
+          "Class -10",
+      },
+      {
+        title: "Intermediate",
+        subTitle: "Sasi Junior College",
+        description:
+          "Science stream",
+      },
+      {
+        title: "Bachelor of Technology",
+        subTitle: "VIT institute of technology, Amaravati.",
+      },
+    ],
+  };
